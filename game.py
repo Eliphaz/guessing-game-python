cbe0a1aeeab79600ccdad02f0e1afeab8861044d
Eliphaz <eliahlander3@gmail.com>
2022-04-05 13:23:12 -0600

    added some error handling

--- game.py
+++ game.py
@@ -8,22 +8,29 @@ print('hi')
 
 name = input('what is your name? ')
 print(f"hello {name}, I'm thinking of a number between 1 and 100")
-correct_num = random.randint(1, 100)
-count = 1
-guess = input('Your guess: ')
-if int(guess):
-    while guess != correct_num:
-        if int(guess) > correct_num:
-            print('too high')
-        if int(guess) < correct_num:
-            print('too low')
-        if int(guess) == correct_num:
-            print(f'congrats {name} thats the correct number!')
-            print(f'it took you {count} guesses!')
-            break
-        count += 1
+
+
+def playgame():
+    correct_num = random.randint(1, 100)
+    count = 1
+    try:
         guess = input('Your guess: ')
+        if int(guess):
+            while guess != correct_num:
+                if int(guess) > correct_num:
+                    print('too high')
+                if int(guess) < correct_num:
+                    print('too low')
+                if int(guess) == correct_num:
+                    print(f'congrats {name} thats the correct number!')
+                    print(f'it took you {count} guesses!')
+                    break
+                count += 1
+                guess = input('Your guess: ')
+
+    except ValueError:
+        print('please enter a integer')
+        playgame()
 
 
-else:
-    print('please enter a integer')
+playgame()
